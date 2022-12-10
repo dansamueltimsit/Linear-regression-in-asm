3fd0cc86e76afbba9f07d647894dfe719f6213e4
dansamueltimsit <119239825+dansamueltimsit@users.noreply.github.com>
2022-12-10 16:55:14 +0100

    Update linearregression.asm

--- linearregression.asm
+++ linearregression.asm
@@ -1,1 +1,23 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;We here implement a linear regression function
+;using asm x86
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;We start with some useful procedures
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;First a linear function taking as parameters a, b and x and returning ax+b
+
+procedure_linear:
+  mov dword ptr [rsp+18h], r8d
+  mov dword ptr [rsp+10h], edx
+  mov dword ptr [rsp+8], ecx
+  push rbp
+  push rdi
+  mov eax, dword ptr [a]
+  imul eax, dword ptr [x]
+  add eax, dword ptr [b]
+  ret
+  
+  
